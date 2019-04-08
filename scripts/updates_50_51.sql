17d035aa7a26272899f55f92a1ae046f0ce82218
Davide Bianchi <davideyeahsure@gmail.com>
2019-04-08 12:03:22 +0200

    Added new field to update script

--- scripts/updates_50_51.sql
+++ scripts/updates_50_51.sql
@@ -1,2 +1,3 @@
 ALTER TABLE ONLY documentscontent drop constraint documentscontent_hostid_key;
 ALTER TABLE ONLY documentscontent ADD CONSTRAINT documentscontent_hostid_key UNIQUE (hostid,language,title);
+ALTER TABLE ONLY documents ADD (published       timestamp       default null);
